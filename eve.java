bd23578a7d195a6b88f35e43066f48455747364a
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-08-19 09:28:19 +0530

    Create eve.java

---
+++ eve.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a;
+		Scanner s = new Scanner(System.in);
+		a=s.nextInt();
+		if(a%2==0)
+		{
+			System.out.println(a);
+		}
+		else
+		{
+			a=a-1;
+		
+		System.out.println(a);
+	}
+	}
+}
