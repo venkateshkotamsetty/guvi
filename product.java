8a9e24c1409ec42f5f539bb76316285a27cf28ca
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-08-19 09:46:11 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,sum=0,b;
+		Scanner s = new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		sum=a*b;
+		if(sum%2==0)
+		System.out.println("even");
+		else
+		System.out.println("odd");
+	}
+}
