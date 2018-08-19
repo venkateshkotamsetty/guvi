fb64cbcaf437fc82601ed7104930249444805283
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-08-19 09:36:16 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,sum=0;
+		Scanner s = new Scanner(System.in);
+		a=s.nextInt();
+		sum=(a)*(a+1);
+		sum=sum/2;
+		System.out.println( sum);
+	}
+}
