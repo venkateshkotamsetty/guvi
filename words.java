18ccab85530db5f8f8a7358f85d0be44e1bcaa19
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-08-20 11:30:56 +0530

    Create words.java

---
+++ words.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int count=1,i;
+		char c;
+		String s1;
+		Scanner s=new Scanner(System.in);
+		s1=s.nextLine();
+		for(i=0;i<s1.length();i++)
+		{
+			c=s1.charAt(i);
+		
+		if(c==' ')
+		{
+			count++;
+			
+		}
+		}
+	
+	
+	System.out.println(count);
+}
+	
+}
