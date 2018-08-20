b04e048475a63bdb6044f3a9592f103ebc0758df
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-08-20 12:53:20 +0530

    Create compare.java

---
+++ compare.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int k=0;
+		Scanner s=new Scanner(System.in);
+		String s1 = s.next();
+		String s2 = s.next();
+		int a=s1.length();
+		int b=s1.length();
+		if(a>=b)
+		{
+		System.out.println(s1);
+		}
+		else
+		{
+		System.out.print(s2);
+		}
+	
+}
+	
+}
