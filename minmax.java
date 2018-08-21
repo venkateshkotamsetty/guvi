d63bafe8b7bd4a7dc78d7d1ed4ecbcffb248b26f
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-08-21 11:01:53 +0530

    Create minmax.java

---
+++ minmax.java
@@ -0,0 +1,38 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,num,max,min;
+		Scanner s= new Scanner(System.in);
+		 num = s.nextInt();
+		int a[]=new int[10];
+		for(i=0;i<num;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		
+		min=a[0];
+		for(i=0;i<num;i++)
+		{
+			if(min>a[i])
+			min=a[i];
+		}
+		System.out.println(min);
+		max=a[0];
+		for(i=0;i<num;i++)
+		{
+			if(max<a[i])
+			max=a[i];
+		}
+		System.out.println(max);
+		
+		
+	}
+}
