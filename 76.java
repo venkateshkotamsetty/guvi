817f63263010c241e2c039fe4e2c3d161986e83f
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-08-24 10:37:38 +0530

    Create 76.java

---
+++ 76.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,s1=0,c=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(int i=2;i<n;i++)
+		{
+			if(n%i==0)
+			{
+			c=1;
+				
+			}
+		}
+		if(c==1)
+		System.out.print("yes");
+		else
+		System.out.print("no");
+		
+	}
+}
