5b7f827e877437bd3b56fd86f9cc02567246c781
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-08-28 17:50:44 +0530

    Create 93.java

---
+++ 93.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int tk,tc;
+		Scanner s=new Scanner(System.in);
+		tc=s.nextInt();
+		tk=tc+273;
+		System.out.print(tk);
+	}
+}
