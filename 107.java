859b8258ab87aff89931a2e7a393ab5b37499ea8
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-08-30 12:32:59 +0530

    Create 107.java

---
+++ 107.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		int d=(a*b);
+		int e=d/c;
+		System.out.print(e);
+		
+	}
+}
