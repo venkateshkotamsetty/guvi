fcf1089ab5524fc60b1489808e356f4d87d56b42
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-09-08 15:22:26 +0530

    Create 102.java

---
+++ 102.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,sum=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		if(n%2==0)
+		{
+			sum=n/2;
+			System.out.print(sum);
+		}
+		else
+		System.out.print(n);
+	}
+}
