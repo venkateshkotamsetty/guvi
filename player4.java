cd17087b91dc0d82f6598c1067c4788b34f2c0a6
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-09-08 19:04:36 +0530

    Create player4.java

---
+++ player4.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner s=new Scanner(System.in);
+		 String n=s.next();
+		 String m=".";
+		 char a[]=n.toCharArray();
+		 for(int i=0;i<n.length();i++)
+		 {
+		 	
+		 }
+		 m=n+m;
+		System.out.print(m);
+		 
+	}
+}
